447458e5d478b8f5ddd5323c9819c95c28792236
markbrutx <68157044+quintbrut@users.noreply.github.com>
2022-06-28 00:16:45 +0600

    Update main.py

--- main.py
+++ main.py
@@ -654,12 +654,10 @@ def telegram_bot_init():
         user = update.message.from_user
         logger.info("User %s started the conversation off_some_one.", user.first_name)
         keyboard = [
-            [
-            ]
         ]
         for x in accounts:
             nick = get_nick_by_account_id(x['account_id'])
-            keyboard[0].append(InlineKeyboardButton(nick, callback_data=x['account_id']))
+            keyboard.append(InlineKeyboardButton(nick, callback_data=x['account_id']))
         reply_markup = InlineKeyboardMarkup(keyboard)
         await update.message.reply_text("Выбирай, Кого вырубаем?", reply_markup=reply_markup)
         return START_ROUTES
